55fc49e6b269b8ee1c1f2e7af510aabd4c854ea1
miyago9267 <miyagomizugen@gmail.com>
2024-03-06 16:07:24 +0800

    feat: using template for bot

--- app.py
+++ app.py
@@ -1,41 +1,12 @@
-import discord
-import discord.emoji
-import random
-import dotenv
+from dotenv import load_dotenv
 import os
-import re
+from src.models.bot import Bot
 
+load_dotenv()
 
-play = ["✊", "✌️", "🖐️"]
-text = ["平手啦智障！", "你輸了 給我去尻尻！", "幹, 你是不是作弊啊！"]
+def bot_start():
+    bot = Bot()
+    bot.run(os.getenv('BOT_TOKEN'))
 
-dotenv.load_dotenv()
-intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print("Bot is ready!")
-    print("Name: {}".format(client.user.name))
-    print("ID: {}".format(client.user.id))
-
-@client.event
-async def on_message(msg):
-    if msg.author == client.user:
-        return
-    # print(msg.content, msg.author)
-    if msg.content in play:
-        player = play.index(msg.content)
-        com = random.randint(0, 2)
-        judge = (player-com+3)%3
-        await msg.channel.send(f'{play[com]}')
-        await msg.channel.send(f'{text[judge]}')
-    if re.search(r'隨機\s', msg.content):
-        st_idx = msg.content.find('隨機 ')
-        query_list = [i for i in msg.content[st_idx+3:].strip(' ').split(' ') if i != '']
-        query_text = msg.content[:st_idx]
-        # print(f'"{msg.content[st_idx+2:]}"','\n', query_list, '➝' ,query_text)
-        await msg.reply(f'隨機 [ {" ".join(query_list)} ]\n{"" if query_text==None else query_text} ➝ **{random.choice(query_list)}**')
-
-client.run(os.getenv("token"))
+if __name__=='__main__':
+    bot_start()
---
+++ src/models/bot.py
@@ -0,0 +1,37 @@
+import discord.ext.commands as commands
+import discord
+import os
+from dotenv import load_dotenv
+from src.cogs.mora import MoraKokoro
+
+load_dotenv()
+
+class Bot(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        intents = discord.Intents.all()
+        super().__init__(
+            command_prefix=os.getenv('COMMAND_PREFIX'),
+            intents=intents,
+            help_command=None
+        )
+
+    async def on_ready(self):
+        await self.add_cog(BotEventsCog(self))
+        await self.add_cog(MoraKokoro(self))
+
+class BotEventsCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Bot is ready!')
+        print('Name: {}'.format(self.bot.user.name))
+        print('ID: {}'.format(self.bot.user.id))
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author == self.bot.user:
+            return
+        if message.content.startswith('!hello'):
+            await message.channel.send('Hello!')
